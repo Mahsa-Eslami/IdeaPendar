a27cd02403aa2b287460c92a08d4efe6a9226517
Mahsa <mahsa.eslami1982@gmail.com>
2022-09-07 05:39:54 +0430

    Upload Mini Project

---
+++ index.js
@@ -0,0 +1,19 @@
+form.addEventListener("submit", function submitFunction(e) {
+  e.preventDefault();
+  var tableBody = document.getElementById("tablebody");
+  const row = document.createElement("tr");
+  var name = document.querySelectorAll("input")[0].value;
+  var family = document.querySelectorAll("input")[1].value;
+  var age = document.querySelectorAll("input")[2].value;
+  tableBody.appendChild(row);
+  row.classList.add("addedRows");
+  row.innerHTML =
+    "<td>" + name + "</td><td>" + family + "</td><td id=age>" + age + "</td>";
+  for (i = 0; i < 3; i++) {
+    document.getElementsByTagName("input")[i].value = "";
+  }
+  var cell = document.querySelectorAll("td");
+  for (i = 0; i < cell.length; i++) {
+    cell[i].classList.add("addedRows");
+  }
+});
